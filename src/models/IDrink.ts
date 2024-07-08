81aa9793ce1de80128fa964b41762b5ec94b108a
Niccar95 <niccar.95@outlook.com>
2024-07-08 21:46:06 +0200

    Added extensions for IDrink interface

--- src/models/IDrink.ts
+++ src/models/IDrink.ts
@@ -3,3 +3,31 @@ export interface IDrink {
   strDrink: string;
   strDrinkThumb: string;
 }
+
+export interface IDrinkIngredients extends IDrink {
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+}
+
+export interface IDrinkInfo extends IDrink {
+  strCategory: string;
+  strAlcoholic: string;
+  strGlass: string;
+}
+
+export interface IDrinkInstructions extends IDrink {
+  strInstructions: string;
+}
