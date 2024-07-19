2dcdd6873173d7704f767e9ca14e513752980e7e
Niccar95 <niccar.95@outlook.com>
2024-07-20 01:09:34 +0200

    added dynamic ingredient list to home page random drink

--- src/components/RandomDrink.tsx
+++ src/components/RandomDrink.tsx
@@ -8,21 +8,34 @@ export const RandomDrink = ({ randomDrink }: IRandomDrinkProps) => {
   return (
     <>
       {randomDrink.length > 0 &&
-        randomDrink.map((drink) => (
-          <article id="randomDrinkCard" key={drink.idDrink}>
-            <div className="imageContainer">
-              <img
-                className="randomDrinkImage"
-                src={drink.strDrinkThumb}
-                alt={drink.strDrink}
-              ></img>
-            </div>
-            <section>
-              <h2>{drink.strDrink}</h2>
-              <p>{drink.strInstructions}</p>
-            </section>
-          </article>
-        ))}
+        randomDrink.map((drink) => {
+          const ingredientKeys = Object.keys(drink).filter(
+            (key) =>
+              key.startsWith("strIngredient") &&
+              drink[key as keyof ICompleteDrinkInfo]
+          );
+
+          return (
+            <article id="randomDrinkCard" key={drink.idDrink}>
+              <div className="imageContainer">
+                <img
+                  className="randomDrinkImage"
+                  src={drink.strDrinkThumb}
+                  alt={drink.strDrink}
+                ></img>
+              </div>
+              <section>
+                <h2>{drink.strDrink}</h2>
+                <ul>
+                  {ingredientKeys.map((key) => (
+                    <li key={key}>{drink[key as keyof ICompleteDrinkInfo]}</li>
+                  ))}
+                </ul>
+                <p>{drink.strInstructions}</p>
+              </section>
+            </article>
+          );
+        })}
     </>
   );
 };
