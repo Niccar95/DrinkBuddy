61838c5d9a2562620946748ca00e1c20093a2472
Niccar95 <niccar.95@outlook.com>
2024-08-11 17:30:57 +0200

    created a hamburger menu for mobile

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,23 @@
 import { NavLink, Outlet } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import { useState } from "react";
 
 export const Layout = () => {
+  const [burgerClass, setBurgerClass] = useState("burgerBar unclicked");
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+    console.log("Menu clicked");
+    if (!isMenuClicked) {
+      setBurgerClass("burgerBar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burgerBar unclicked");
+      setMenuClass("menu hidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
   return (
     <>
       <header>
@@ -9,7 +25,25 @@ export const Layout = () => {
           <img className="logoImg" src={logo}></img>
         </div>
         <nav>
-          <ul>
+          <div className="hamburgerMenu" onClick={updateMenu}>
+            <div className={burgerClass}></div>
+            <div className={burgerClass}></div>
+            <div className={burgerClass}></div>
+          </div>
+          <div className={menuClass}>
+            <ul className="navList">
+              <li>
+                <NavLink to={"/"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/drinks"}>Drinks</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/favourites"}>Favourites</NavLink>
+              </li>
+            </ul>
+          </div>
+          <ul className="navList">
             <li>
               <NavLink to={"/"}>Home</NavLink>
             </li>
