457cf25d16f27bd081866fe0b99b797479555382
Niccar95 <niccar.95@outlook.com>
2024-08-13 18:59:49 +0200

    created a theme context

---
+++ src/contexts/ThemeContext.ts
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+
+export interface ITheme {
+  name: string;
+  backgroundColor: string;
+  textColor: string;
+}
+
+export interface IThemes {
+  light: ITheme;
+  dark: ITheme;
+}
+
+export const themes: IThemes = {
+  light: {
+    name: "Light",
+    backgroundColor: "white",
+    textColor: "black",
+  },
+  dark: {
+    name: "Dark",
+    backgroundColor: "black",
+    textColor: "white",
+  },
+};
+
+export const ThemeContext = createContext<ITheme>(themes.dark);
